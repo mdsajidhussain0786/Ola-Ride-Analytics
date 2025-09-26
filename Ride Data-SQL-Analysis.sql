74f37dc6a9c09963760585a45be81069ccb55791
MD SAJID HUSSAIN <hussainms244@gmail.com>
2025-09-27 02:13:41 +0530

    Add files via upload

---
+++ Ride Data-SQL-Analysis.sql
@@ -0,0 +1,20 @@
+ #1. Retrieve all successful bookings:
+     SELECT*FROM Successful_Bookings;
+ #2. Find the average ride distance for each vehicle type:
+     SELECT*FROM ride_distance_for_each_vehicle;
+ #3. Get the total number of cancelled rides by customers:
+	 SELECT*FROM cancelled_rides_by_customer;
+ #4. List the top 5 customers who booked the highest number of rides:
+	 SELECT*FROM top_5_customers;
+ #5. Get the number of rides cancelled by drivers due to personal and car-related issues:
+	 SELECT*FROM rides_cancelled_by_drivers;
+ #6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
+	 SELECT* FROM max_and_min_driver_rating;
+ #7. Retrieve all rides where payment was made using UPI:
+	 SELECT* FROM payment_with_UPI;
+ #8. Find the average customer rating per vehicle type:
+	 SELECT* FROM avg_customer_rating;
+ #9. Calculate the total booking value of rides completed successfully:
+	 SELECT* FROM total_success_booking_value;
+ #10. List all incomplete rides along with the reason:
+	 SELECT* FROM incomplete_rides_with_reason;
